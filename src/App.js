bb1ce7859ff3ffee1066a0886cefc929cc78a54f
Manh An <an49248@gmail.com>
2021-10-04 14:46:51 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './default.scss'
+import Header from './components/Header'
+import Homepage from './pages/Homepage'
 function App() {
   return (
     <div className="App">
-       h
+       <Header />
+      <div className = 'main'>
+        <Homepage />
+       </div>
     </div>
   );
 }
